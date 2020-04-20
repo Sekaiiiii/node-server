3369a75ec544ca0426ba4c6fb6f2d4d7472f7c6f
sekaiiiii <sekaiiiii@163.com>
2020-04-20 21:49:46 +0800

    提交后端代码模板

--- api/android/login.js
+++ api/android/login.js
@@ -7,11 +7,19 @@
 "use strict"
 
 const express = require("express");
+const pool = require("../../tool/pool.js");
+
 
 const router = express.Router();
 
 router.all("/",function(req,res){
-
+    pool.query("select * from role",function(err,data,fileds){
+        if(err){
+            console.error(err);
+            return;
+        }
+        console.log(data);
+    })
 })
 
 module.exports = router;
