1d1377182877750dbe78366db3fe7e4ed6193c3f
sekaiiiii <sekaiiiii@163.com>
2020-04-20 20:43:33 +0800

    提交

---
+++ api/android/login.js
@@ -0,0 +1,17 @@
+/*
+* author:谢奇
+* create_day:2020-04-20
+* modified_day:2020-04-20
+* function:路由配置
+*/
+"use strict"
+
+const express = require("express");
+
+const router = express.Router();
+
+router.all("/",function(req,res){
+
+})
+
+module.exports = router;
