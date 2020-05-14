1d5f87c25e03cd7dc0b25522f7d85fc392c9f858
sekaiiiii <qq738359456@gmail.com>
2020-05-14 17:44:46 +0800

    update

--- api/web/get_mysql_dump.js
+++ api/web/get_mysql_dump.js
@@ -24,13 +24,13 @@ router.get("/", function (req, res, next) {
     fs.readdir(dumpPath, function (err, dir_list) {
         if (err) {
             console.error(err);
-            return return_obj.fail("602", "获取数据库备份列表失败");
+            return res.send(return_obj.fail("602", "获取数据库备份列表失败"));
         }
         console.log(dir_list);
-        return_obj.success({
+        res.send(return_obj.success({
             msg: "获取数据库备份列表成功",
             dump_list: dir_list
-        })
+        }));
     })
 })
 
