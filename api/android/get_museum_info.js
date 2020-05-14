c47a525388c375d9c96efa8256ff321f07215b12
sekaiiiii <qq738359456@gmail.com>
2020-05-14 15:28:03 +0800

    update get_museum_info android api

--- api/android/get_museum_info.js
+++ api/android/get_museum_info.js
@@ -46,8 +46,15 @@ router.get("/", function (req, res, next) {
                 museum,position
             where
                 museum.position_id = position.id
+                ${req.query.name ? "and museum.name like ?" : ""}
             `;
-            pool.query(sql, function (err, museum_list, fileds) {
+
+            let param_list = [];
+            if (req.query.name) {
+                param_list.push("%" + req.query.name + "%");
+            }
+
+            pool.query(sql, param_list, function (err, museum_list, fileds) {
                 if (err) {
                     console.error(err);
                     return done(new Error("200"), null);
@@ -56,6 +63,10 @@ router.get("/", function (req, res, next) {
             });
         },
         function (museum_list, done) {
+            if (museum_list.length == 0) {
+                return done(null, museum_list);
+            }
+
             let museum_id_set = new Set();
             let museum_id_list = new Array();
             museum_list.forEach(museum => {
@@ -71,19 +82,119 @@ router.get("/", function (req, res, next) {
                     console.error(err);
                     return done(new Error("200"), null);
                 }
-                done(null, museum_list, image_list);
+
+                for (let i = 0; i < museum_list.length; i++) {
+                    museum_list[i].image_list = new Array();
+                    for (let j = 0; j < image_list.length; j++) {
+                        if (image_list[j].museum_id == museum_list[i].id) {
+                            museum_list[i].image_list.push(image_list[j].file);
+                        }
+                    }
+                    return done(null, museum_list);
+                }
             });
         },
-        function (museum_list, image_list, done) {
-            for (let i = 0; i < museum_list.length; i++) {
-                museum_list[i].image_list = new Array();
-                for (let j = 0; j < image_list.length; j++) {
-                    if (image_list[j].museum_id == museum_list[i].id) {
-                        museum_list[i].image_list.push(image_list[j].file);
-                    }
-                }
+        function (museum_list, done) {
+            //0名字首字母 
+            if (req.query.order_by == 0) {
+                museum_list.sort(function (a, b) {
+                    if (a.name > b.name) return -1;
+                    return 1;
+                })
+                return done(null, museum_list);
             }
-            done(null, museum_list);
+            //1展览数量
+            if (req.query.order_by == 1) {
+                let sql = `
+                select
+                    museum.id as id,
+                    count(*) as exhibition_num
+                from
+                    museum,
+                    exhibition
+                where
+                    museum.id = exhibition.museum_id
+                group by
+                    museum.id
+                `;
+                pool.query(sql, function (err, exhibition_num_list, fileds) {
+                    if (err) {
+                        console.error(err);
+                        return done(new Error("200"));
+                    }
+
+                    for (let i = 0; i < museum_list.length; i++) {
+                        museum_list[i].exhibition_num = 0;
+                        for (let j = 0; j < exhibition_num_list.length; j++) {
+                            if (exhibition_num_list[j].id == museum_list[i].id) {
+                                museum_list[i].exhibition_num = exhibition_num_list[j].exhibition_num;
+                            }
+                        }
+                    }
+
+                    museum_list.sort(function (a, b) {
+                        if (a.exhibition_num > b.exhibition_num) {
+                            return -1;
+                        }
+                        return 1;
+                    })
+
+                    return done(null, museum_list);
+                })
+
+            }
+            //2藏品数量
+            if (req.query.order_by == 2) {
+                let sql = `
+                select
+                    museum.id as id,
+                    count(*) as collection_num
+                from
+                    museum,
+                    collection
+                where
+                    museum.id = collection.museum_id
+                group by
+                    museum.id
+                `;
+                pool.query(sql, function (err, collection_num_list, fileds) {
+                    if (err) {
+                        console.error(err);
+                        return done(new Error("200"));
+                    }
+
+                    for (let i = 0; i < museum_list.length; i++) {
+                        museum_list[i].collection_num = 0;
+                        for (let j = 0; j < collection_num_list.length; j++) {
+                            if (collection_num_list[j].id == museum_list[i].id) {
+                                museum_list[i].collection_num = collection_num_list[j].collection_num;
+                            }
+                        }
+                    }
+
+                    museum_list.sort(function (a, b) {
+                        if (a.collection_num > b.collection_num) {
+                            return -1;
+                        }
+                        return 1;
+                    })
+                    return done(null, museum_list);
+                })
+            };
+            //3用户综合评分
+            if (req.query.order_by == 3) {
+                museum_list.sort(function (a, b) {
+                    if (a.environment_score + a.exhibition_score + a.service_score >=
+                        b.environment_score + b.exhibition_score + b.service_score) {
+                        return -1;
+                    }
+                    return 1;
+                })
+                return done(null, museum_list);
+            }
+            if (!req.query.order_by) {
+                return done(null, museum_list);
+            }
         }
     ], function (error, museum_list) {
         if (error) {
