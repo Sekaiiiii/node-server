60eba7a0d111ac3ca848a7f149d4a118b12dc77c
sekaiiiii <qq738359456@gmail.com>
2020-05-21 21:08:19 +0800

    update start_scrapy.js

--- api/group2/start_scrapy.js
+++ api/group2/start_scrapy.js
@@ -13,18 +13,36 @@ const { exec } = require('child_process');
 const router = express.Router();
 
 //运行helloworld脚本
-//处理业务代码
-router.get("/", function (req, res, next) {
-    exec(`cd ~/Project/SE-group2/Museum_News_Collection/museum_news_spider`)
-    exec(`/home/ubuntu/.local/bin/scrapyd-deploy`)
 
-    command = `curl http://localhost:6800/schedule.json -d project=museum_news_spider -d spider=${req.query.spider}`
-    if (req.query.museum)
-        command += `-d museum=${req.query.museum}`
-    if (req.query.startTime)
-        command += `-d startTime=${req.query.startTime}`
-    if (req.query.endTime)
-        command += `-d endTime=${req.query.endTime}`
+router.get("/start_server", function (req, res, next) {
+    exec(path.join(__dirname, "..", "..", "shell", "scrapy_server.sh"), function (req, res, next) {
+        if (err) {
+            return res.send(err);
+        }
+        return res.send({
+            stdout: stdout,
+            stderr: stderr
+        });
+    });
+})
+
+
+
+
+router.get("/start_spider", function (req, res, next) {
+    // if (req.query.startTime)
+    //     command += `-d museum=${req.query.museum}`
+    // if (req.query.startTime)
+    //     command += `-d startTime=${req.query.startTime}`
+    // if (req.query.endTime)
+    //     command += `-d endTime=${req.query.endTime}`
+    
+    command = `${path.join(__dirname,"..","..","shell","spider.sh")} 
+                 ${req.query.museum ? `${req.query.museum}` : "" } 
+                 ${req.query.startTime ? `${req.query.startTime}` : "" }
+                 ${req.query.endTime ? `${req.query.endTime}` : "" }
+            `;
+
     exec(command, (err, stdout, stderr) => {
         if (err) {
             return res.send(err);
